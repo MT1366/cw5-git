0b224a524a754f168e683663f2d9c4a56a28f2da
milad <miladtavassoli13@gmail.com>
2023-03-30 16:43:15 +0430

    first Commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// Question One
+
+// let obj = {
+//   1: 5,
+//   2: 7,
+//   3: 0,
+//   4: 0,
+//   5: 0,
+//   6: 0,
+//   7: 0,
+//   8: 0,
+//   9: 0,
+//   10: 0,
+//   11: 0,
+//   12: 0,
+// };
+
+// let finallResult = [];
+
+// let arr = Object.entries(obj);
+
+// console.log(arr);
+
+// Question Two
+
+// function duplicate(arry) {
+//   let obj = {};
+//   for (let element of arry) {
+//     if (obj[element]) {
+//       obj[element]++;
+//     } else {
+//       obj[element] = 1;
+//     }
+//   }
+//   return obj;
+// }
+// let myarry = ["a", "a", "b", "b", "c"];
+// console.log(duplicate(myarry));
+
+// Question Three
+
+// function findtext(text) {
+//   textorginal = "";
+//   for (let char of text) {
+//     if (char >= "a" && char <= "z") {
+//       textorginal += char;
+//     }
+//   }
+//   return textorginal;
+// }
+// console.log(findtext("ADWQWDcADWaFAWEt"));
+
+// Question Four
+
+// function compareObject(ob1, ob2) {
+//   const keyob1 = Object.keys(ob1);
+//   const keyob2 = Object.keys(ob2);
+//   for (let i = 0; i < keyob1.length; i++) {
+//     if (keyob1[i] !== keyob2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// const object1 = {
+//   name: "Benny",
+//   phone: "3325558745",
+//   email: "benny@edabit.com",
+// };
+// const object2 = {
+//   name: "Jason",
+//   phone: "9853759720",
+//   email: "jason@edabit.com",
+// };
+// console.log(compareObject(object1, object2));
+
+// Question Five
